b4c82a6f2ab0733873cadfafa6abc4afe2a253fa
Andreas Kissner <andikiss500@gmail.com>
2025-12-06 19:01:10 +0100

    Refactor Style .css Part 3

--- scripts/version_loader.js
+++ scripts/version_loader.js
@@ -1,7 +1,6 @@
 const version = new Date().getTime(); 
 document.getElementById("styleLink").href = `./style.css?v=${version}`;
 const style = document.getElementById("styleLink");
-
 style.onload = () => {
   document.body.style.visibility = "visible";
 };
